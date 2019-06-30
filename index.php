dcf51af09405f8a74543386f714f8cf41188dda1
NeT32 <isaias_finger@hotmail.com>
2019-06-29 23:43:11 -0300

    Initial Web Page

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>Isaias’ Personal Website</title>
+    <meta name="description" content="Isaias’ Personal Website" />
+    <meta name="author" content="Isaias" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="./styles.css" />
+  </head>
+  <body>
+    <section class="content">
+      <picture>
+        <source type="image/webp" srcset="./assets/isaias.webp" />
+        <img
+          class="isaias-image"
+          src="./assets/isaias.jpeg"
+          alt="Isaias’ Profile Picture"
+        />
+      </picture>
+      <h1>
+        Hello, I’m <strong>Isaias Finger</strong>, an Developer. I develop apps using, Node.js, Vue.js, Magento, Laravel, and JAVA.
+      </h1>
+      <div class="links">
+        <a
+          href="https://github.com/net32"
+          target="_blank"
+          rel="noopener"
+        >
+          <img src="./assets/github.svg" alt="GitHub logo" />
+          GitHub
+        </a>
+        <a
+          href="https://twitter.com/isaiasfinger"
+          target="_blank"
+          rel="noopener"
+        >
+          <img src="./assets/twitter.svg" alt="Twitter logo" />
+          Twitter
+        </a>
+        <a
+          href="https://www.linkedin.com/in/isaiasfinger/"
+          target="_blank"
+          rel="noopener"
+        >
+          <img src="./assets/linkedin.webp" alt="Linkedin logo" />
+          Linkedin
+        </a>
+      </div>
+    </section>
+  </body>
+</html>
